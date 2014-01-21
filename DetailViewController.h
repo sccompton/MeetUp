24d919a9ebe1140a40544108eabfd0115c4ade8f
Stephen Compton <sccompton@me.com>
2014-01-20 19:27:06 -0600

    first commit

---
+++ DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MeetUp
+//
+//  Created by Stephen Compton on 1/20/14.
+//  Copyright (c) 2014 Stephen Compton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
